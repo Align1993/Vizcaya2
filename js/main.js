60d7aab2d0c68af4e02a0c9cb255898fc0f9745e
Kelvin Brito <britok30@gmail.com>
2019-11-15 12:19:26 -0500

    ultra

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-$(function() {
-  $("body").niceScroll({
-    scrollspeed: 60
-  });
-});
 
 ScrollReveal().reveal(".navbar", {
   duration: 3000,
@@ -30,9 +25,74 @@ ScrollReveal().reveal(".lead-p", {
   delay: 400
 });
 
+ScrollReveal().reveal(".sub-heading", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+ScrollReveal().reveal(".design-head", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+ScrollReveal().reveal(".design-head2", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+
+ScrollReveal().reveal(".design-head3", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
 ScrollReveal().reveal(".ione", {
   duration: 3000,
   origin: "bottom",
   distance: "5rem",
   delay: 1000
 });
+
+ScrollReveal().reveal(".video", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 1000
+});
+
+ScrollReveal().reveal(".video1", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 1000
+});
+
+ScrollReveal().reveal(".job1", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 300
+});
+
+ScrollReveal().reveal(".job2", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+ScrollReveal().reveal(".job3", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 500
+});
+
