e7a56829a73a534400815c9b11bad79f7a1cf54b
Kelvin Brito <britok30@gmail.com>
2019-11-14 22:31:16 -0500

    map

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+  $("html").niceScroll({
+    scrollspeed: 40
+  });
+});
+
+ScrollReveal().reveal(".navbar", {
+  duration: 2000,
+  origin: "bottom",
+  delay: 1000
+});
+
+ScrollReveal().reveal(".lead1", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "10rem",
+  delay: 100
+});
+
+ScrollReveal().reveal(".lead2", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "10rem",
+  delay: 300
+});
+
+ScrollReveal().reveal(".lead3", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "10rem",
+  delay: 500
+});
