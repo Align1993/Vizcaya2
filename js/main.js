2acd3a1b2dfedf05b50561bf720d683b652cda3f
Kelvin Brito <britok30@gmail.com>
2019-11-15 12:30:27 -0500

    footer

--- js/main.js
+++ js/main.js
@@ -96,3 +96,124 @@ ScrollReveal().reveal(".job3", {
   delay: 500
 });
 
+ScrollReveal().reveal(".l1", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 200
+});
+
+ScrollReveal().reveal(".l2", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 250
+});
+
+ScrollReveal().reveal(".l3", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 300
+});
+
+ScrollReveal().reveal(".l4", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 350
+});
+
+ScrollReveal().reveal(".l5", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+ScrollReveal().reveal(".l6", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 200
+});
+
+ScrollReveal().reveal(".l7", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 250
+});
+
+ScrollReveal().reveal(".l8", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 300
+});
+
+ScrollReveal().reveal(".l9", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 350
+});
+
+ScrollReveal().reveal(".l10", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 400
+});
+
+ScrollReveal().reveal(".l11", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 450
+});
+
+ScrollReveal().reveal(".l12", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 500
+});
+
+ScrollReveal().reveal(".l13", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 500
+});
+
+
+ScrollReveal().reveal(".f1", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 500
+});
+
+
+ScrollReveal().reveal(".f2", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 600
+});
+
+ScrollReveal().reveal(".f3", {
+  duration: 3500,
+  origin: "bottom",
+  distance: "5rem",
+  delay: 650
+});
+
+
+
+
+
+
+
+
